1769d703b1a04bd91852ce11699d25bc469e45ea
Dustin <dustinkaban@gmail.com>
2020-05-30 00:13:18 -0700

    Update Day3_HourGlassSum.cpp

--- Day3_HourGlassSum.cpp
+++ Day3_HourGlassSum.cpp
@@ -1,3 +1,10 @@
+//*************PROBLEM
+//    Complete the function hourglassSum in the editor below. 
+//    It should return an integer, the maximum hourglass sum in the array.
+//    hourglassSum has the following parameter(s):
+//      - arr: an array of integers
+
+
 #include <bits/stdc++.h>
 
 using namespace std;
