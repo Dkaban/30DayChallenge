c28732b0bda0d39e3ad1df221f8ec2fbd9edd5e5
Dustin <dustinkaban@gmail.com>
2020-05-30 08:43:04 -0700

    Update Day4_PrintFScanF.cpp

--- Day4_PrintFScanF.cpp
+++ Day4_PrintFScanF.cpp
@@ -1,3 +1,6 @@
+//Print each element on a new line in the same order it was received as input. 
+//Note that the floating point value should be correct up to 3 decimal places and the double to 9 decimal places.
+
 #include <iostream>
 #include <cstdio>
 
