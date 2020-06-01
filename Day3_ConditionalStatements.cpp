b0468edf2cbc99b3f68e09188673502d6073ea29
Dustin <dustinkaban@gmail.com>
2020-06-01 15:06:59 -0700

    Update Day3_ConditionalStatements.cpp

--- Day3_ConditionalStatements.cpp
+++ Day3_ConditionalStatements.cpp
@@ -1,3 +1,14 @@
+// THE PROBLEM
+// ***************************
+// Given an integer,n, perform the following conditional actions:
+// If n is odd, print Weird
+// If n is even and in the inclusive range of 2 to 5, print Not Weird
+// If n is even and in the inclusive range of 6 to 20, print Weird
+// If n is even and greater than 20, print Not Weird
+// Solution Created By: Dustin Kaban
+// Date: May 29th, 2020
+// ***************************
+
 #include <bits/stdc++.h>
 
 using namespace std;
