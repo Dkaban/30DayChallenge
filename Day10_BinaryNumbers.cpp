658371f0469887b64755df54c2ca42df3536a3ac
Dustin <dustinkaban@gmail.com>
2020-06-05 15:37:14 -0700

    Update Day10_BinaryNumbers.cpp

--- Day10_BinaryNumbers.cpp
+++ Day10_BinaryNumbers.cpp
@@ -1,4 +1,4 @@
-  // THE PROBLEM
+// THE PROBLEM
 // ***************************
 // Given a base-10 integer, n, convert it to binary (base-2). 
 // Then find and print the base-10 integer denoting the maximum number of 
