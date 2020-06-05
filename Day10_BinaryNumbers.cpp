4bfb71c24533bf40291fd58172d359b38282461f
Dustin <dustinkaban@gmail.com>
2020-06-05 15:34:31 -0700

    Create Day10_BinaryNumbers.cpp

---
+++ Day10_BinaryNumbers.cpp
@@ -0,0 +1,51 @@
+  // THE PROBLEM
+// ***************************
+// Given a base-10 integer, n, convert it to binary (base-2). 
+// Then find and print the base-10 integer denoting the maximum number of 
+// consecutive 1's in n's binary representation.
+// Solution Created By: Dustin Kaban
+// Date: June 5th, 2020
+// ***************************
+
+#include <bits/stdc++.h>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+void convertToBinary(int n)
+{
+    string binaryString = bitset<32>(n).to_string();
+
+    int x = binaryString.length(); 
+    char char_array[x + 1]; 
+    strcpy(char_array, binaryString.c_str()); 
+
+    int result = 0;
+    int count = 0;
+    for (int i = 0; i < (int)sizeof(char_array)-1; i++) 
+    { 
+        if (char_array[i] == '0')
+        {
+            count = 0; 
+        }
+        else
+        {
+            count++;
+            result = max(result, count); 
+        } 
+    } 
+
+    cout << result << endl;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    convertToBinary(n);
+
+    return 0;
+}
