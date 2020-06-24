d14aa57bc9d289c62aee9ecbe795db537b824331
Dustin <dustinkaban@gmail.com>
2020-06-24 09:18:50 -0700

    Update Day29_BitwiseAnd.cpp

--- Day29_BitwiseAnd.cpp
+++ Day29_BitwiseAnd.cpp
@@ -3,7 +3,7 @@
 // Given set S={1,2,3...N}. Find two integers, A and B (where A<B), from set S 
 // such that the value of A&B is the maximum possible and also less than a given integer, K. In this case, & represents the bitwise AND operator.
 // Solution Created By: Dustin Kaban
-// Date: June 5th, 2020
+// Date: June 24th, 2020
 // ***************************
 
 #include <bits/stdc++.h>
